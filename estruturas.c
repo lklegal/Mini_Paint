b77d107578cacbecaea9024f41ec53eccc1c9ad7
lklegal <110703055+lklegal@users.noreply.github.com>
2023-09-20 08:33:14 -0300

    Removi struct não utilizado

--- estruturas.c
+++ estruturas.c
@@ -2,10 +2,6 @@
 #include <stdlib.h>
 #include "cabecalhos.h"
 
-typedef struct{
-    float dados[3][3];
-}matriz;
-
 obj** criarListaDeObjetos(){
 	obj **listaDeObjetos = (obj**)malloc(sizeof(obj*));
 	if(listaDeObjetos != NULL){
